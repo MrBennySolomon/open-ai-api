0acf38a20686899abff2eebf037a4157e005aab8
MRBENNYSOLOMON <mr.benny.solomon@gmail.com>
2023-05-11 22:04:49 +0300

    open-ai-api-translation

--- index.js
+++ index.js
@@ -1,37 +1,40 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
 
-import { Configuration, OpenAIApi } from 'openai';
 dotenv.config();
 
 const config = new Configuration({
-	apiKey: process.env.OPEN_AI_API_KEY
+  apiKey: process.env.OPEN_AI_API_KEY
 });
 
 const openai = new OpenAIApi(config);
 
-const runPrompt = async () => {
-	const prompt = `
-        write me a joke about a cat and a bowl of pasta. Return response in the following parsable JSON format:
+const runPrompt = async (question) => {
+  const prompt = question;
 
-        {
-            "Q": "question",
-            "A": "answer"
-        }
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: prompt,
+    max_tokens: 2048,
+    temperature: 1
+  });
 
-    `;
+  const parsableJSONresponse = response.data.choices[0].text;
 
-	const response = await openai.createCompletion({
-		model: "text-davinci-003",
-		prompt: prompt,
-		max_tokens: 2048,
-		temperature: 1,
-	});
-
-	const parsableJSONresponse = response.data.choices[0].text;
-	const parsedResponse = JSON.parse(parsableJSONresponse);
-
-	console.log("Question: ", parsedResponse.Q);
-	console.log("Answer: ", parsedResponse.A);
+  if (
+    question.includes("from english to hebrew") ||
+    question.includes("from english to arabic")
+  ) {
+    return parsableJSONresponse.split("").reverse().join("");
+  } else {
+    return parsableJSONresponse;
+  }
 };
 
-runPrompt();
+const question_EN_HE = `translate from english to hebrew : "Hello World 1"`;
+const question_EN_AR = `translate from english to arabic : "Hello World 2"`;
+const question_HE_EN = `translate from hebrew to english : "שלום עולם 3"`;
+const question_HE_AR = `translate from hebrew to arabic : "שלום עולם 4"`;
+
+const result = await runPrompt(question_EN_HE);
+console.log(result);
